e7d4fb3b13541192b00e25e3dd4d0138a3f3e2ba
SeeringC <nguyentanabc91@gmail.com>
2025-06-03 12:29:04 +0700

    fix

--- rcpsp_j90_no_bound_only_time_per_instance.py
+++ rcpsp_j90_no_bound_only_time_per_instance.py
@@ -22,7 +22,8 @@ import sys
 import csv
 import time
 from pathlib import Path
-
+from google.cloud import storage
+import os
 
 def solve_rcpsp(data_file):
     """
@@ -193,6 +194,17 @@ def main():
 
     print(f"\nAll done! Results written to {output_file}")
 
+    # Tên bucket mà bạn đã tạo
+    bucket_name = "rcpsp-results-bucket"
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+
+    local_path = "result/j90_no_bound_600s.csv"
+    blob_name = f"results/{os.path.basename(local_path)}"  # ví dụ "results/j30_no_bound_1200s.csv"
+
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(local_path)
+    print(f"Uploaded {local_path} to gs://{bucket_name}/{blob_name}")
 
 if __name__ == "__main__":
     main()
